8737c5b24fbcfa30fe85ce68a1e4bb40dea234e9
Jakub Knapik <128633057+Knaappz@users.noreply.github.com>
2024-02-20 16:23:06 +0100

    some changes v5

--- src/pages/components/header/comp/Logo.jsx
+++ src/pages/components/header/comp/Logo.jsx
@@ -3,7 +3,7 @@ import { Link } from "react-router-dom";
 function FajneForumLogo() {
     return(
         <Link to='/'>
-            <img src="../assets/logonav-ff.png" alt="Nie ma loga, dupa..." />
+            <img src="https://github.com/knaappz/fajne_forum_web/blob/master/assets/logonav-ff.png?raw=true" alt="Image not found." />
         </Link>
     )
 }
